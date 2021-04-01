caa05b7afa975168c353e50f9e11a5f42a9effd7
Seth Delorme <delorme.s.m@gmail.com>
2021-03-31 20:26:39 -0500

    adds signup

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
     <div id="nav">
       <router-link to="/">Home</router-link>
       |
+      <router-link to="/signup">Sign Up</router-link>
+      |
       <router-link to="/about">About</router-link>
       |
       <router-link to="/fun">Fun</router-link>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Fun from "../views/Fun.vue";
+import Signup from "../views/Signup.vue";
 
 Vue.use(VueRouter);
 
@@ -10,6 +11,11 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/signup",
+    name: "Signup",
+    component: Signup,
   },
   {
     path: "/fun",
