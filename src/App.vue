ee38a60620d6c52dc5d7302b9a7ab8fce8c9e142
Seth Delorme <delorme.s.m@gmail.com>
2021-03-31 19:42:07 -0500

    adds fun page and route

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@
       <router-link to="/">Home</router-link>
       |
       <router-link to="/about">About</router-link>
+      |
+      <router-link to="/fun">Fun</router-link>
     </div>
     <router-view />
   </div>
